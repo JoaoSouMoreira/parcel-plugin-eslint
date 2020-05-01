c604df5a4fac47145c35c4955058785fb1aa0fdd
Joao Moreira <joao.moreira@funeralzone.co.uk>
2020-05-01 12:23:41 +0100

    Fix asset

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 module.exports = function (bundler) {
-  bundler.addAssetType('vue', require.resolve('./ESLintAsset.js'));
+  bundler.addAssetType('vue', require.resolve('./VueESLintAsset.js'));
   bundler.addAssetType('js', require.resolve('./ESLintAsset.js'));
   bundler.addAssetType('jsx', require.resolve('./ESLintAsset.js'));
 };
